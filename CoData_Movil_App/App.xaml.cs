893e36d89f389e1d5f0a85a254990cfc464d81ee
Bryan24007 <bryanbhdzp24@gmail.com>
2025-11-15 11:39:21 -0700

    Agregar archivos de proyecto.

---
+++ CoData_Movil_App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CoData_Movil_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CoData_Movil_App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CoData_Movil_App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
